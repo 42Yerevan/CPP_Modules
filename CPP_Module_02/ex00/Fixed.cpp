6635e3931bb8a5f2b0cd60c80bffb487b054b8ad
Vladimir Musaelyan <vmusaely@c2r4s4.42yerevan.am>
2022-05-19 19:36:36 +0400

    new

--- CPP_Module_02/ex00/Fixed.cpp
+++ CPP_Module_02/ex00/Fixed.cpp
@@ -27,10 +27,12 @@ Fixed& Fixed::operator=(const Fixed& fixed)
 
 int Fixed::getRawBits( void ) const
 {
+	std::cout << "getRawBits member function called" << std::endl;
 	return this->fixed_point;
 }
 
 void Fixed::setRawBits( int const raw )
 {
+	std::cout << "setRawBits member function called" << std::endl;
 	this->fixed_point = raw;
 }
--- CPP_Module_02/ex00/Fixed.hpp
+++ CPP_Module_02/ex00/Fixed.hpp
@@ -12,7 +12,7 @@ class Fixed
 		Fixed();
 		~Fixed();
 		Fixed(Fixed& fixed);
-		Fixed& Fixed::operator=(const Fixed& fixed);
+		Fixed& operator=(const Fixed& fixed);
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
 };
